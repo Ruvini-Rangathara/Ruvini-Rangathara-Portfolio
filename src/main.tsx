29b8e7d5160de37f23c3f7f0002cab5ed3be3073
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-06-22 23:19:23 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import App from './App.tsx'
+import './index.css'
+import {ReactDOM} from "react";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
